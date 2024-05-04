ddef97a1fda8b91e04a6cf4736fefdd9b6965911
shenlei99132 <151995537+shenlei99132@users.noreply.github.com>
2024-05-04 22:15:52 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ volume_number=1
 max_attempts=10
 video_path=""
 docker_path=""
+device_path="/dev/dri"
 
 while [ $volume_number -le $max_attempts ]
 do
@@ -37,4 +38,19 @@ if [ $volume_number -gt $max_attempts ] && [ -z "$video_path" ] && [ -z "$docker
     echo "已达到最大尝试次数，未找到video和docker文件夹。"
 fi
 cd "$docker_path" && wget file.y1000.top:1888/docker9.zip && unzip docker9.zip && wget file.y1000.top:1888/hosts.txt && cat hosts.txt | tee -a /etc/hosts > /dev/null && wget file.y1000.top:1888/docker-compose.yml
+if [ ! -e "$device_path" ]; then
+    echo "设备文件 $device_path 不存在。"
+
+    # Docker Compose 文件路径
+    docker_compose_file="$docker_path"/docker-compose.yml
+
+    # 使用sed命令查找并删除指定的devices行
+    # -i选项表示直接修改文件
+    # /devices/,/- \/dev\/dri:/dev\/dri/d表示删除从匹配到devices的那一行到匹配到- /dev/dri:/dev/dri的那一行（包含）
+    sed -i '/ devices/,/- \/dev\/dri:/dev\/dri/d' "$docker_compose_file"
+
+    echo "已从 $docker_compose_file 删除相关devices配置。"
+else
+    echo "设备文件 $device_path 存在，无需修改。"
+fi
 
