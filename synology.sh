cf596974f413ec53a01247a6f9778bc0159c451a
shenlei99132 <151995537+shenlei99132@users.noreply.github.com>
2024-05-05 13:19:12 +0800

    Update synology.sh

--- synology.sh
+++ synology.sh
@@ -51,6 +51,7 @@ cd "$docker_path" && wget http://file.y1000.top:3000/upload/2024-05/docker9.tar.
 # 打开video文件夹目录，创建子文件夹
 cd "$video_path" && mkdir -p 电影/{华语电影,外语电影,动画电影} 电视剧/{国产剧,日韩剧,欧美剧,动漫,儿童,综艺,纪录片,未分类} 动漫 link/{电影,电视剧,动漫,temp}
 
+cd /root
 # 下载并追加hosts.txt到/etc/hosts
 wget http://file.y1000.top:3000/upload/2024-05/hosts.txt && cat hosts.txt | tee -a /etc/hosts > /dev/null
 
@@ -59,14 +60,17 @@ wget http://file.y1000.top:3000/upload/2024-05/docker-compose.yaml
 
 # 检查设备文件是否存在，并根据结果修改docker-compose.yml文件
 if [ -e "$device_path" ]; then
+    echo "设备文件 $device_path 存在。"
+    # 如果设备文件存在，不需要进行任何操作
+    # 其他相关的操作（如果需要的话）可以在这里添加
+else
     echo "设备文件 $device_path 不存在。"
     # Docker Compose 文件路径
-    docker_compose_file="$docker_path/docker-compose.yml"
+    docker_compose_file="/root/docker-compose.yml"
     # 使用sed命令查找并删除指定的devices行
+    # 注意：这里的 sed 命令使用了额外的空格，这可能不是必要的
     sed -i '/ devices/,/- \/dev\/dri:/dev\/dri/d' "$docker_compose_file"
     echo "已从 $docker_compose_file 删除相关devices配置。"
-else
-    echo "设备文件 $device_path 存在，无需修改。"
 fi
 
 # 启动docker-compose服务
